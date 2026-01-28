01c9438fc6fddeb39756ac706c05d8a182678a52
Angel Aquino <156721843+AngelEmilioAquino@users.noreply.github.com>
2026-01-28 18:49:37 -0400

    add Angel Favicon logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,25 +8,24 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'MiNegocio - Dashboard de Ventas',
+  title: 'DNI Dashboard - Prueba tecnica',
   description: 'Panel de control para gestionar ventas y clientes',
-  generator: 'v0.app',
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: 'public/logoNew.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: 'public/logoNew.png',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: 'public/logoNew.png',
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: 'public/logoNew.png',
   },
 }
 
